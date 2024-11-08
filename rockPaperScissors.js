382e2871c2ba9e5f63efccc870e87948ad13901d
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-08 15:47:26 +0000

    Create js file and divide project into tasks

---
+++ rockPaperScissors.js
@@ -0,0 +1,21 @@
+console.log("Wagwan g")
+
+// A Rock Paper Scissors game played entirely in the console.
+
+// Logic to get computer choice
+
+
+
+// Logic to get human choice
+
+
+
+// Declare the player score variables
+
+
+
+// Write the logic to play a single round
+
+
+
+// Write the logic to play an entire game
