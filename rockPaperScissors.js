c9d6d8c251cc21379a8be31a247c06f641a47530
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-10 15:00:57 +0000

    Write up computer choice function pseudocode

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -8,7 +8,17 @@ console.log("Wagwan g")
 
 // Logic to get human choice
 
+function getComputerChoice {
+    // Function that randomly returns a string value of "rock", "scissors", or, "paper"
 
+    // Generate random number between 0 and 1
+    // Multiply generated value by 100
+
+    // If 0-33, return rock
+    // If 34-66, return paper
+    // If 67-100, return scissors
+    // Return string value
+}
 
 // Declare the player score variables
 
