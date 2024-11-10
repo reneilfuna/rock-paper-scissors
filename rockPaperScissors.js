2dc7ad8a5b6ddc5f5548a4573f0a06dab8516ba1
Reneil <123956661+reneilfuna@users.noreply.github.com>
2024-11-10 15:01:53 +0000

    Implement computer choice code

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -13,11 +13,25 @@ function getComputerChoice {
 
     // Generate random number between 0 and 1
     // Multiply generated value by 100
-
+    let randomNumber = (Math.random() * 100);
     // If 0-33, return rock
     // If 34-66, return paper
     // If 67-100, return scissors
     // Return string value
+    if (randomNumber >= 0 && randomNumber <= 33){
+        choice = "rock";
+        return choice;
+    }
+    else if (randomNumber >= 34 && randomNumber <= 66){
+        choice = "paper";
+        return choice;
+    }
+    else if (randomNumber >= 67 && randomNumber <= 100){
+        choice = "scissors";
+        return choice;
+    }
+    else
+        console.log("Wait a minute...");
 }
 
 // Declare the player score variables
